3390165e0c7c19dfbe0268894de1c371f7ce3420
Matt Cone <mattcone@users.noreply.github.com>
2024-01-22 16:00:55 -0700

    Set response headers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 /// <reference types="@fastly/js-compute" />
 
-function handler(event) {
-  return fetch(event.request, {
+const handler = async (event) => {
+  // get the request from the client
+  const req = event.request
+ 
+  const backendResponse = await fetch(req, {
     backend: "vcl-origin",
     cacheOverride: new CacheOverride("pass")
   });
+ 
+  // Add headers to the response back to the client
+  backendResponse.headers.append("x-tacos", "🌮🌮🌮")
+ 
+  // send the backend response to the client
+  return backendResponse;
  }
  
  addEventListener("fetch", event => event.respondWith(handler(event)));
