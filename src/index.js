c2bf5122efd2644872bd31ea1630a6129e0824d6
Matt Cone <mattcone@users.noreply.github.com>
2024-01-22 16:04:45 -0700

    Create custom 404 response

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ const handler = async (event) => {
     cacheOverride: new CacheOverride("pass")
   });
  
-  // Add headers to the response back to the client
-  backendResponse.headers.append("x-tacos", "🌮🌮🌮")
+  // Handle 404s with a custom response
+  if (backendResponse.status == 404) {
+    return new Response("This is our custom Compute 404 page", {
+      status: 404,
+    });
+  }
  
-  // send the backend response to the client
-  return backendResponse;
+  // If status is not 404, send the backend response to the client
+  if (backendResponse.status != 404) {
+    // Add headers to the response back to the client
+    backendResponse.headers.append("x-tacos", "🌮🌮🌮")
+    return backendResponse;
+  }
  }
  
  addEventListener("fetch", event => event.respondWith(handler(event)));
